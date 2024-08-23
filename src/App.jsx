b7d434e74b4dc789fb55bc861300eb755be1ed4e
Ronak Sharma <rsvk1819@gmail.com>
2024-08-23 13:42:38 +0530

    created the state for different types of form of complaint and enquiry

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
-import { useState } from 'react'
-import './App.css'
+import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
 
 function App() {
-
-
+  
   return (
     <>
-     <h1 className='text-3xl  font-bold'>
-      Welcome to SIH24😊🔥🔥🔥🔥🔥🔥
-     </h1>
-      
+      <h1 className="text-3xl  font-bold ">Welcome to SIH24😊🔥🔥🔥🔥🔥🔥</h1>
+
+    
     </>
-  )
+  );
 }
 
-export default App
+export default App;

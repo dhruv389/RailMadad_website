71f7c39cdce70758b29adaa3ebb02048e6af65b0
Dhaval rathod <drcoder389@gmail.com>
2024-08-23 11:09:54 +0530

    Welcome to SiH24

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     <h1 className='text-3xl  font-bold'>
+      Welcome to SIH24😊🔥🔥🔥🔥🔥🔥
+     </h1>
+      
+    </>
+  )
+}
+
+export default App
